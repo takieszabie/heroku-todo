f405d790d7048e79fc4fd6feb8eed42b2f42f479
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-12 17:04:18 +0200

    add form postmethod written

--- src/main/java/com/greenfoxacademy/herokutodo/Controllers/ToDoController.java
+++ src/main/java/com/greenfoxacademy/herokutodo/Controllers/ToDoController.java
@@ -1,10 +1,16 @@
 package com.greenfoxacademy.herokutodo.Controllers;
 
+import com.greenfoxacademy.herokutodo.Model.ToDo;
 import com.greenfoxacademy.herokutodo.Repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.constraints.Positive;
 
 @Controller
 @RequestMapping("/todo")
@@ -18,9 +24,14 @@ public class ToDoController {
     model.addAttribute("todos", toDoRepository.findAll());
     return "todolist";
   }
-  @RequestMapping(value = {"/add"})
+  @GetMapping("/add")
   public String showAddForm(Model model) {
     return "addform";
+  }
+
+  @PostMapping("/add")
+  public String addSubmit(@ModelAttribute ToDo toDo) {
+    return "result";
   }
 
 }
